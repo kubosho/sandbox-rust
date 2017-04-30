cb54732d33b2e5abf5a07ac562d665e2d918efaf
kubosho <ta2@o2p.jp>
2017-04-30 23:32:39 +0900

    feat(*): add return func

---
+++ return-func/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    add_one(5);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ return-func/target/debug/return-func.d
@@ -0,0 +1,1 @@
+/Users/kubosho/src/github.com/kubosho/sandbox-rust/return-func/target/debug/return-func: /Users/kubosho/src/github.com/kubosho/sandbox-rust/return-func/src/main.rs
